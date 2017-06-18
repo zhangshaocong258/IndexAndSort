33edfe3611ab5ba8c33d74bb6761dd845ae2ea19
zhangshaocong258 <597001494@qq.com>
2017-06-18 17:08:24 +0800

    变量名修改

--- src/main/java/mybatis/Forward.java
+++ src/main/java/mybatis/Forward.java
@@ -9,36 +9,38 @@ public class Forward implements Comparable<Forward> {
     private String title;//标题
     private String url;//网页地址
     private String description;//摘要
-    private int quality;//权重
+    private int quality1;//关注者，关注者，关注者
+    private int quality2;//被浏览，回答，问题
     private String keyWords;//关键词
     private String TF;
 
     public Forward() {
     }
 
-    public Forward(String title, String url, String description, int quality, String keyWords) {
+    public Forward(String title, String url, String description, int quality1, String keyWords) {
         this.title = title;
         this.url = url;
         this.description = description;
-        this.quality = quality;
+        this.quality1 = quality1;
         this.keyWords = keyWords;
     }
 
-    public Forward(String title, String url, String description, int quality, String keyWords, String TF) {
+    //question专用
+    public Forward(String title, String url, String description, int quality1, String keyWords, String TF) {
         this.title = title;
         this.url = url;
         this.description = description;
-        this.quality = quality;
+        this.quality1 = quality1;
         this.keyWords = keyWords;
         this.TF = TF;
     }
 
     @Override
     public int compareTo(Forward forward) {
-        if (forward.quality == quality) {
+        if (forward.quality1 == quality1) {
             return 0;
         } else {
-            return forward.quality > quality ? 1 : -1;
+            return forward.quality1 > quality1 ? 1 : -1;
         }
     }
 
@@ -66,12 +68,20 @@ public class Forward implements Comparable<Forward> {
         this.keyWords = keyWords;
     }
 
-    public int getQuality() {
-        return quality;
+    public int getQuality1() {
+        return quality1;
     }
 
-    public void setQuality(int quality) {
-        this.quality = quality;
+    public void setQuality1(int quality1) {
+        this.quality1 = quality1;
+    }
+
+    public int getQuality2() {
+        return quality2;
+    }
+
+    public void setQuality2(int quality2) {
+        this.quality2 = quality2;
     }
 
     public String getTitle() {
